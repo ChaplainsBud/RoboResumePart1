2ac3c63999d16a4ada5d6cc65a5d3d0e636e5c35
GBTC440010UR <GBTC440010UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-07 09:03:14 -0500

    Accept README.MD

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,3 +6,33 @@ public class Main {
 	// write your code here
     }
 }
+
+
+/*
+Can I do a while loop, while unfinished: true;
+loop back through q's until everyone is answered
+
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Enter name: ")
+String name = sc.nextLine();
+
+System.out.println("Enter email address: ")
+String name = sc.nextLine();
+
+degree level
+major
+university name
+graduation year
+1 work experience
+company
+job title
+start date
+end date
+1 job description
+3 skills
+skill name
+rating/proficiency
+
+
+ */
