c9a4df3081e5a44daa0f38c816744bc62baac991
GBTC440010UR <GBTC440010UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-07 12:00:54 -0500

    Still working on while loops to solve empty answers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+// Last feature: Ensure all fields have been entered, minimally
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -16,86 +18,96 @@ public class Main {
 
         ArrayList<String> data = new ArrayList<String>();
 
-        Scanner sc = new Scanner(System.in);
+            Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter a name: ");
-        String name = sc.nextLine();
-        data.add(name);
-        System.out.println("Enter an email address: ");
-        String email = sc.nextLine();
-        data.add(email);
-        System.out.println("Enter highest diploma: ");
-        String diploma = sc.nextLine();
-        data.add(diploma);
-        System.out.println("Enter major ");
-        String major = sc.nextLine();
-        data.add(major);
-        System.out.println("Enter university name: ");
-        String uni = sc.nextLine();
-        data.add(uni);
-        System.out.println("Enter graduation year: ");
-        String yr = sc.nextLine();
-        data.add(yr);
-        System.out.println("Enter company: ");
-        String co = sc.nextLine();
-        data.add(co);
-        System.out.println("Enter job title: ");
-        String ti = sc.nextLine();
-        data.add(ti);
-        System.out.println("Enter start date: ");
-        String start = sc.nextLine();
-        data.add(start);
-        System.out.println("Enter end date: ");
-        String end = sc.nextLine();
-        data.add(end);
-        System.out.println("Enter job description: ");
-        String desc = sc.nextLine();
-        data.add(desc);
-        System.out.println("Enter skill 1: ");
-        String sk1 = sc.nextLine();
-        data.add(sk1);
-        System.out.println("Enter rating/ranking: ");
-        String rk1 = sc.nextLine();
-        data.add(rk1);
-        System.out.println("Enter skill 2: ");
-        String sk2 = sc.nextLine();
-        data.add(sk2);
-        System.out.println("Enter rating/ranking: ");
-        String rk2 = sc.nextLine();
-        data.add(rk2);
-        System.out.println("Enter skill 3: ");
-        String sk3 = sc.nextLine();
-        data.add(sk3);
-        System.out.println("Enter rating/ranking: ");
-        String rk3 = sc.nextLine();
-        data.add(rk3);
+            System.out.println("Enter a name: ");
+            String name = sc.nextLine();
+            data.add(name);
+            System.out.println("Enter an email address: ");
+            String email = sc.nextLine();
+            data.add(email);
+            System.out.println("Enter highest diploma: ");
+            String diploma = sc.nextLine();
+            data.add(diploma);
+            System.out.println("Enter major ");
+            String major = sc.nextLine();
+            data.add(major);
+            System.out.println("Enter university name: ");
+            String uni = sc.nextLine();
+            data.add(uni);
+            System.out.println("Enter graduation year: ");
+            String yr = sc.nextLine();
+            data.add(yr);
+            System.out.println("Enter company: ");
+            String co = sc.nextLine();
+            data.add(co);
+            System.out.println("Enter job title: ");
+            String ti = sc.nextLine();
+            data.add(ti);
+            System.out.println("Enter start date: ");
+            String start = sc.nextLine();
+            data.add(start);
+            System.out.println("Enter end date: ");
+            String end = sc.nextLine();
+            data.add(end);
+            System.out.println("Enter job description: ");
+            String desc = sc.nextLine();
+            data.add(desc);
+            System.out.println("Enter skill 1: ");
+            String sk1 = sc.nextLine();
+            data.add(sk1);
+            System.out.println("Enter rating/ranking: ");
+            String rk1 = sc.nextLine();
+            data.add(rk1);
+            System.out.println("Enter skill 2: ");
+            String sk2 = sc.nextLine();
+            data.add(sk2);
+            System.out.println("Enter rating/ranking: ");
+            String rk2 = sc.nextLine();
+            data.add(rk2);
+            System.out.println("Enter skill 3: ");
+            String sk3 = sc.nextLine();
+            data.add(sk3);
+            System.out.println("Enter rating/ranking: ");
+            String rk3 = sc.nextLine();
+            data.add(rk3);
 
-    // how do I populate the constructor with data?
-    Applicant app = new Applicant(name, email, diploma, major, uni, yr, co, ti, start, end, desc, sk1, rk1, sk2, rk2, sk3, rk3);
+        Applicant app = new Applicant(name, email, diploma, major, uni, yr, co, ti, start, end, desc, sk1, rk1, sk2, rk2, sk3, rk3);
 
-       // System.out.println(app.getName());
 
         System.out.println("=============================================================");
-        System.out.println(app.getName());
-        System.out.println(app.getEmail());
-        System.out.println("");
-        System.out.println("");
-        System.out.println("Education");
-        System.out.println(app.getDiploma() + " in " + app.getMajor());
-        System.out.println(app.getUni() + " , " + app.getYr());
-        System.out.println("");
-        System.out.println("");
-        System.out.println("Experience");
-        System.out.println(app.getCo());
-        System.out.println(app.getTi() + " , from " + app.getStart() + " to " + app.getEnd());
-        System.out.println("Duty 1 " + app.getDesc());
-        System.out.println("");
-        System.out.println("");
-        System.out.println("Skills");
-        System.out.println(app.getSk1() + " , " + app.getRk1());
-        System.out.println(app.getSk2() + " , " + app.getRk2());
-        System.out.println(app.getSk3() + " , " + app.getRk3());
+            System.out.println(app.getName());
+            System.out.println(app.getEmail());
+            System.out.println("");
+            System.out.println("");
+            System.out.println("Education");
+            System.out.println(app.getDiploma() + " in " + app.getMajor());
+            System.out.println(app.getUni() + " , " + app.getYr());
+            System.out.println("");
+            System.out.println("");
+            System.out.println("Experience");
+            System.out.println(app.getCo());
+            System.out.println(app.getTi() + " , from " + app.getStart() + " to " + app.getEnd());
+            System.out.println("Duty 1 " + app.getDesc());
+            System.out.println("");
+            System.out.println("");
+            System.out.println("Skills");
+            System.out.println(app.getSk1() + " , " + app.getRk1());
+            System.out.println(app.getSk2() + " , " + app.getRk2());
+            System.out.println(app.getSk3() + " , " + app.getRk3());
 
+
+
+        }
+
+
+
+
+        // check ArrayList for any empty indexes; if a single empty space, redo
+
+    // how do I populate the constructor with data?
+
+       // System.out.println(app.getName());
 
 //        System.out.println(data.get(0));
 //        System.out.println(data.get(1));
@@ -118,7 +130,7 @@ public class Main {
 //        System.out.println(data.get(15) + " , " + data.get(16));
 
     }
-}
+
 
 
 /*
